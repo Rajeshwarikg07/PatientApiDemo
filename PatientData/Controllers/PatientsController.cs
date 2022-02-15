638d749c6be7a2087b1537add5771eab4b4f3038
rajeshwari <rajeshwari.kg17@outlook.com>
2022-02-15 13:02:08 +0530

    inital

---
+++ PatientData/Controllers/PatientsController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PatientData.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientsController : ControllerBase
+    {
+        public static ArrayList PatientData = new ArrayList()
+        {
+            "Tom"
+        };
+        
+
+        [HttpGet]
+        [Route("GetAllPatients")]
+        public ArrayList GetAllPatients()
+        {
+            return PatientData;
+        }
+
+        [HttpGet]
+        [Route("GetPatientById")]
+
+        public string GetPatient(int id)
+        {
+            return PatientData[id].ToString();
+        }
+
+        [HttpPost]
+        [Route("SaveNewPatient")]
+        public ArrayList SaveNewUser([FromQuery] string patient)
+        {
+            PatientData.Add(patient);
+            return PatientData;
+        }
+
+        [HttpPut]
+        [Route("UpdatePatient")]
+        public ArrayList UpdatePatient(int id, string patient)
+        {
+            PatientData[id] = patient;
+            return PatientData;
+        }
+
+        [HttpDelete]
+        [Route("DeletePatient")]
+        public ArrayList DeletePatient(int id)
+        {
+            PatientData.RemoveAt(id);
+            return PatientData;
+        }
+    }
+}
